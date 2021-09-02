1860f2f72efca93bbfdb99187275a6e082ed6b8c
crisMtech95 <cmg.95.col@hotmail.com>
2021-09-02 17:54:30 -0400

    fixing the styling in the search component

--- react-app/src/components/Search/index.js
+++ react-app/src/components/Search/index.js
@@ -8,17 +8,23 @@ import "./Search.css"
 const Search = () => {
     const searchRes = useSelector(state => Object.values(state.search))
     // const searchRes = useSelector(state => state.searchRes)
-
+    
     return (
         <div className="Search__container">
             <div className="Search__bigResContainer">
                 <div className="search__title">
-                    <h2>Search Results</h2>
+                    <h2>Search Results:</h2>
                 </div>
                 <div className="Search__resContainer">
-                    {searchRes?.map(post => (
+
+                    {searchRes.length > 0 ?
+                        searchRes.map(post => (
                         <Post key={post.id} post={post}/>
-                        ))}
+                        )):
+                        <div className="Search__noResults">
+                            <p>Found 0 matches</p>
+                        </div>
+                        }
                 </div>
             </div>
             <div className="search__mapsDiv">

bc90f2117868822c86c0efff731145d32fb84a91
crisMtech95 <cmg.95.col@hotmail.com>
2021-09-02 16:34:05 -0400

    details

--- react-app/src/components/Review/index.js
+++ react-app/src/components/Review/index.js
@@ -33,6 +33,7 @@ function Review({ review }) {
         const clickOutSide = (e) => {
             if (!editReviewRef?.current?.contains(e.target)) {
                 setShowEditReview(false)
+                setShowEditBtn(false)
             }
         }
         document.addEventListener("mousedown", clickOutSide)

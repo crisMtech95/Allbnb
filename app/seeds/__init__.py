1dbd0d3973e5384eec10ba3057ed03cf05124d6c
crisMtech95 <cmg.95.col@hotmail.com>
2023-09-14 11:34:07 -0400

    changing all the seed files to add db scheme

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -5,6 +5,8 @@ from .posts import seed_posts, undo_posts
 from .reservations import seed_reservations, undo_reservations
 from .reviews import seed_reviews, undo_reviews
 from .images import seed_images, undo_images
+from app.models.db import db, environment, SCHEMA
+
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
 seed_commands = AppGroup('seed')
@@ -13,6 +15,11 @@ seed_commands = AppGroup('seed')
 # Creates the `flask seed all` command
 @seed_commands.command('all')
 def seed():
+    if environment == 'production':
+        # Before seeding, truncate all tables prefixed with schema name
+        db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
+        # Add a truncate command here for every table that will be seeded.
+        db.session.commit()
     seed_users()
     seed_categories()
     seed_posts()
--- migrations/versions/a86bf9c46ac0_.py
+++ migrations/versions/a86bf9c46ac0_.py
@@ -1,12 +1,16 @@
 """empty message
 
 Revision ID: a86bf9c46ac0
-Revises: 
+Revises:
 Create Date: 2021-07-31 15:29:45.032535
 
 """
 from alembic import op
 import sqlalchemy as sa
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
+
 
 
 # revision identifiers, used by Alembic.
@@ -77,6 +81,8 @@ def upgrade():
     sa.ForeignKeyConstraint(['userId'], ['users.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
+    if environment == "production":
+        op.execute(f"ALTER TABLE <table_name> SET SCHEMA {SCHEMA};")
     # ### end Alembic commands ###
 
 

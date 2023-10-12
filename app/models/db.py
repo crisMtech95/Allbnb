4e1f3f307ccbbb463c7949a20818e202c8c5960a
crisMtech95 <cmg.95.col@hotmail.com>
2023-10-12 19:48:36 -0400

    fixing indentation in the init file

--- app/models/db.py
+++ app/models/db.py
@@ -8,6 +8,7 @@ SCHEMA = os.environ.get('SCHEMA')
 
 db = SQLAlchemy()
 
+
 # add function to add a prefix to table names in production environment only
 def add_prefix_for_prod(attr):
     if environment == "production":
